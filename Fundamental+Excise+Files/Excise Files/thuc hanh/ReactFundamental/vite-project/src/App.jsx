12158f3638e7b73f15baf3604077a3b214cbf647
Liem <ngotrongliem2004@gmail.com>
2025-02-03 23:04:58 +0700

    init du an

---
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
+export default App
