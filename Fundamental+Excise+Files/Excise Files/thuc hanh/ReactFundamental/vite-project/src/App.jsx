4011c1b8f681876ed28cfe7324d7ac6c1ee7d03e
Liem <ngotrongliem2004@gmail.com>
2025-02-05 14:54:57 +0700

    passing object to child component

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -5,10 +5,18 @@ import Hero from './component/Hero';
 
 
 const App = () => {
+
+  const itemObj = {
+    name: "Ngô Trọng Liêm",
+    age: 21,
+    city: "Hồ Chí Minh"
+  };
+
+
   return (
     <div>
       <h1>This is from App</h1>
-      <Hero title="This is from Hero title" des="in detail information!" />
+      <Hero item={itemObj} />
     </div>
   )
 }
--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/component/Hero.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/component/Hero.jsx
@@ -1,15 +1,13 @@
-
-import HerroImg from './HerroImg';
 const Hero = (props) => {
+  const item = props.item || {}; // Dùng object rỗng nếu item không tồn tại
 
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <h1 className="text-5xl font-bold ">This is from Hero</h1>
-      <h1>{props.title}</h1>
-      <h1>{props.des}</h1>
-      <HerroImg />
+      <h1 className="text-5xl font-bold">This is from Hero</h1>
+      <p>Name: {item.name || 'No Name Provided'}</p>
+      <p>Age: {item.age || 'No Age Provided'}</p>
+      <p>City: {item.city || 'No City Provided'}</p>
     </div>
   );
 }
-
 export default Hero;
