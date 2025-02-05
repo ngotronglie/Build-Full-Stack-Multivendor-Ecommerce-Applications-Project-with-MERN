4703cafacdaf246d9b70a32c11e5599b6cbd6c0f
Liem <ngotrongliem2004@gmail.com>
2025-02-05 15:17:51 +0700

    managing form submit

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -1,23 +1,24 @@
 
-import Hero from './component/Hero';
+// import Hero from './component/Hero';
 
 
 
 
 const App = () => {
 
-  const BtnClick = () => {
-    alert('btn click')
+  const PostFormData = (e) => {
+    e.preventDefault();
+    console.log('Form Submitted')
   }
 
   return (
     <div>
       <h1>This is from App</h1>
-      <button onClick={() => {
-        alert('click btn in app')
-      }}>Click btn in app</button>
-      <button onClick={BtnClick}>click btn in app 2</button>
-      <Hero item={BtnClick} />
+      <form onSubmit={PostFormData}>
+        <input type="text" placeholder='Enter name: ' />
+        <button type="submit">Submit</button>
+      </form>
+
     </div>
   )
 }
