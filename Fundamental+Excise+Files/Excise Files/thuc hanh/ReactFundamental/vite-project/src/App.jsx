1bad6c3c8cffe6eb623eb7ea18a6074da39d071f
Liem <ngotrongliem2004@gmail.com>
2025-02-05 09:20:06 +0700

    JSX conditional rendering using if else

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -1,25 +1,32 @@
 
-
+  const loginStatusBtn = (status) =>{
+    if (status) {
+      return (
+        <div>
+          <h1>login status</h1>
+          <button>login</button>
+        </div>
+      );
+    } else {  
+      return (
+        <div>
+          <h1>logout status</h1>
+          <button>logout</button>
+        </div>
+      );
+    }
+  }
 
 
 
 const App = () => {
-  const status = false;
-  if (status) {
-    return (
-      <div>
-        <h1>login status</h1>
-        <button>login</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>logout status</h1>
-        <button>logout</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>App Component</h1>
+      {loginStatusBtn(true)}
+      {loginStatusBtn(false)}
+    </div>
+  )
 }
 
 export default App;
