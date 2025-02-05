9738a42096ad31e9b1a83096f3af219f9bf1e52e
Liem <ngotrongliem2004@gmail.com>
2025-02-05 09:32:36 +0700

    JSX conditional rendering using temary operator

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -4,17 +4,16 @@
 
 const App = () => {
   const status = true;
-  switch (status) {
-    case true:
-      return <button>true</button>
+  return (
+    <div>
+      {
+        status ? <button>true</button>
+          :
+          <button>false</button>
+      }
+    </div>
+  )
 
-    case false:
-      return <button>false</button>
-    default:
-      return <button>default</button>
-
-  }
-  
 }
 
 export default App;
