1fc88539e5ad398df92d7df7e6ee96f89e63640e
Liem <ngotrongliem2004@gmail.com>
2025-02-07 15:55:09 +0700

    [hook] useRef working with attribute

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
 
   // let myHeadline = document.getElementById('myHeadline');
   let myHeadline = useRef()
+  let myImage = useRef()
 
 
   const change = () => {
@@ -11,12 +12,21 @@ const App = () => {
     // myHeadline.current.innerHTML = '<ul><li> A</li><li>B</li></ul >'
   }
 
+  const changeImage = () => {
+    myImage.current.src = 'https://placehold.co/600x400/orange/white'
+    myImage.current.setAttribute('width', '200px')
+    myImage.current.setAttribute('height', '200px')
+  }
+
   return (
     <div>
       {/* <h1 id="myHeadline"></h1> */}
       <h1 ref={myHeadline}></h1>
       {/* <h1 ref={(h1) => myHeadline = h1}></h1> bug khong fix dc */}
+
+      <img ref={myImage} src="https://placehold.co/600x400" alt="image for https://placehold.co/" />
       <button onClick={change}>Change! Click me</button>
+      <button onClick={changeImage}>Change Image</button>
     </div>
   );
 }
