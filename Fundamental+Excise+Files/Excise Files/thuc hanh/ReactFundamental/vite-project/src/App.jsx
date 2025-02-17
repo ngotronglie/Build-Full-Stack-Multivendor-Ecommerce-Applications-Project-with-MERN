92bd6576ec0e14024095cdbb5c8fada9a4705dc6
Liem <ngotrongliem2004@gmail.com>
2025-02-17 15:21:52 +0700

    [hook] useRef working with input Element

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -1,35 +1,25 @@
-import { useRef } from 'react';
+
+import { useRef } from "react";
 
 const App = () => {
-
-  // let myHeadline = document.getElementById('myHeadline');
-  let myHeadline = useRef()
-  let myImage = useRef()
-
+  let firstName , lastName  = useRef();
+  // let firstName = useRef();
+  // let lastName = useRef();
 
   const change = () => {
-    myHeadline.current.innerHTML = 'xin chao, toi dang su dung useRef()';
-    // myHeadline.current.innerHTML = '<ul><li> A</li><li>B</li></ul >'
+    // console.log(firstName.current.value);
+    // console.log(lastName.current.value);
+    let fName = firstName.current.value
+    let lName = lastName.current.value
+    alert(fName + " " + lName)
   }
-
-  const changeImage = () => {
-    myImage.current.src = 'https://placehold.co/600x400/orange/white'
-    myImage.current.setAttribute('width', '200px')
-    myImage.current.setAttribute('height', '200px')
-  }
-
   return (
     <div>
-      {/* <h1 id="myHeadline"></h1> */}
-      <h1 ref={myHeadline}></h1>
-      {/* <h1 ref={(h1) => myHeadline = h1}></h1> bug khong fix dc */}
-
-      <img ref={myImage} src="https://placehold.co/600x400" alt="image for https://placehold.co/" />
-      <button onClick={change}>Change! Click me</button>
-      <button onClick={changeImage}>Change Image</button>
+      <input ref={firstName} type="text" placeholder="First Name " /> <br />
+      <input ref={lastName} type="text" placeholder="Last Name " /> <br />
+      <button onClick={change}>click mel</button>
     </div>
-  );
+  )
 }
 
 export default App;
-
