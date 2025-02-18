fc06a37a38061f3ce4320d780580105437a689ec
Liem <ngotrongliem2004@gmail.com>
2025-02-18 22:09:45 +0700

    [hook] useRef Caching Expensive Computation

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -2,19 +2,27 @@
 import { useRef } from "react";
 
 const App = () => {
+  let APIData = useRef(null);
+  let myPTag = useRef();
 
-  let myHeadLine = useRef();
-  const change = () => {
-    myHeadLine.current.classList.remove('text-success');
-    myHeadLine.current.classList.add('text-danger');
+  const fetchData = async () => {
+    const response = await fetch("https://dummyjson.com/products")
+    APIData.current = await response.json();
+  }
+
+  const showData = () => {
+    // show data from state
+    myPTag.current.innerText = JSON.stringify(APIData.current);
   }
   return (
     <div>
-      <h1 className="text-success" ref={myHeadLine}> this is head line</h1>
-
-      <button onClick={change} className="btn btn-primary">click mel</button>
+      <p ref={myPTag}></p>
+      <button onClick={fetchData} className="btn btn-success">call api</button>
+      <button onClick={showData} className="btn btn-primary">show data</button>
     </div>
   )
 }
 
 export default App;
+// vao network kiem tra status 200 ok
+// phai fetch api trươc moi lay duoc data
