b77269883d5e20ff55c54cc6959d59d4ddf4f3be
Liem <ngotrongliem2004@gmail.com>
2025-02-20 07:22:09 +0700

    [hook] useState working with immutable Object

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/vite-project/src/App.jsx
@@ -1,16 +1,42 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-  const changeNumber = () => {
-    setNumber(number - 1);
+
+  const [myObj, setMyObject] = useState(
+    {
+      key1: "value1",
+      key2: "value2",
+      key3: "value3",
+    }
+  );
+  const change = () => {
+    setMyObject(
+      {
+        key1: "new value1",
+        key2: "new value2",
+        key3: "new value3",
+      }
+    );
+  };
+  const change2 = () => {
+    setMyObject(
+      prevOBJ => ({
+        ...prevOBJ,
+        key1: "hi, how are you",
+        key2: "I'm fine",
+        key3: "I'm fine too",
+      })
+    )
   }
+
+
   return (
     <div>
-      <h1>Number: {number}</h1>
-
-      <button onClick={() => setNumber(number + 1)} className="btn btn-primary">function change + </button> 
-      <button onClick={changeNumber} className="btn btn-primary">function change -</button>
+      <h1>{myObj.key1}</h1>
+      <h1>{myObj.key2}</h1>
+      <h1>{myObj.key3}</h1>
+      <button type="button" onClick={change} className='btn btn-primary'>Click</button>
+      <button type="button" onClick={change2} className='btn btn-primary'>change 2</button>
     </div>
   )
 }
