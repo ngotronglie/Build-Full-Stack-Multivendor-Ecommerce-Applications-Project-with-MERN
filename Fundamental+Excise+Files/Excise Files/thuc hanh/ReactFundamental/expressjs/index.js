0323a8be784380761b305fed0217fca0ab3c6410
Liem <ngotrongliem2004@gmail.com>
2025-02-22 08:42:27 +0700

    response set cookies

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/headerResponse', function (req, res) {
-    res.append('name',"ngo trong liem");
-    res.append('city',"Thai Binh");
-    res.append('age',18);
-    res.status(201).end("this is response hesader");
+app.get('/setCookieResponse', function (req, res) {
+    res.cookie('name',"ngo trong liem");
+    res.cookie('city',"Thai Binh");
+    res.cookie('age',18);
+    res.status(201).end("this is response cookie");
 })
 app.listen(8000, function () {
     console.log('Example app listening http://localhost:8000/');
