a87252d080df3c2eeb74fa2407403cc5bb3ce881
Liem <ngotrongliem2004@gmail.com>
2025-02-22 08:47:11 +0700

    response clear cookies

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
 
+//response set cookie response
+
 app.get('/setCookieResponse', function (req, res) {
     res.cookie('name',"ngo trong liem");
     res.cookie('city',"Thai Binh");
     res.cookie('age',18);
     res.status(201).end("this is response cookie");
 })
+
+// response clear cookie response
+app.get('/deleteCookieResponse', function (req, res) {
+    res.clearCookie('name');
+    res.clearCookie('city');
+    res.clearCookie('age');
+    res.status(201).end("clear cookie"); 
+})
+
 app.listen(8000, function () {
     console.log('Example app listening http://localhost:8000/');
 });
