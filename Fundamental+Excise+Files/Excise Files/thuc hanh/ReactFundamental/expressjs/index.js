b3e423af1e9a48d3b30df6768395abeaed47cd3b
Liem <ngotrongliem2004@gmail.com>
2025-02-22 08:49:50 +0700

    working with request

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
@@ -1,23 +1,13 @@
 var express = require('express');
 var app = express();
 
-//response set cookie response
-
-app.get('/setCookieResponse', function (req, res) {
-    res.cookie('name',"ngo trong liem");
-    res.cookie('city',"Thai Binh");
-    res.cookie('age',18);
-    res.status(201).end("this is response cookie");
-})
-
-// response clear cookie response
-app.get('/deleteCookieResponse', function (req, res) {
-    res.clearCookie('name');
-    res.clearCookie('city');
-    res.clearCookie('age');
-    res.status(201).end("clear cookie"); 
-})
-
+// request method 
+/**
+ * post()
+ * get()
+ * put()
+ * delete()
+ */
 app.listen(8000, function () {
     console.log('Example app listening http://localhost:8000/');
 });
