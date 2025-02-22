5030e4965f5e81d4eefa7958891b60a04df0c9cd
Liem <ngotrongliem2004@gmail.com>
2025-02-22 08:57:02 +0700

    get resquest with url query

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
@@ -8,6 +8,14 @@ var app = express();
  * put()
  * delete()
  */
+
+app.get('/', function(req, res) {
+   let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    res.end('Hello ' + firstName + ' ' + lastName);
+});
+
+
 app.listen(8000, function () {
     console.log('Example app listening http://localhost:8000/');
 });
