a38a60b8a7923b44a5a9004db114bc907fdfe4f9
Liem <ngotrongliem2004@gmail.com>
2025-02-22 07:47:27 +0700

    response status code

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
@@ -13,6 +13,15 @@ app.post('/contact', function(req, res) {
 app.delete('/shop', function(req, res) {
     res.send('shop page!');
 });
+app.get('/one', function(req, res) {
+    res.send('this is simple string response !');
+});
+app.post('/two', function(req, res) {
+    res.send('this is post simple string response !');
+});
+app.get('/end', function(req, res) {
+    res.end('this is simple end string response !');
+});
 
 app.listen(8000, function () {
     console.log('Example app listening http://localhost:8000/');
