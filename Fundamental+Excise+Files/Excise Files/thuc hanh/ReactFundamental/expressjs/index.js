b52b10efcfcc3c2f7d629fead6317d1032c658ea
Liem <ngotrongliem2004@gmail.com>
2025-02-22 08:37:39 +0700

    response header

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/expressjs/index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/usa', function (req, res) {
-    res.redirect('http://localhost:8000/india');
-});
-
-app.get('/india', function (req, res) {
-    res.send('this is india page! ');
-});
-
+app.get('/headerResponse', function (req, res) {
+    res.append('name',"ngo trong liem");
+    res.append('city',"Thai Binh");
+    res.append('age',18);
+    res.status(201).end("this is response hesader");
+})
 app.listen(8000, function () {
     console.log('Example app listening http://localhost:8000/');
 });
