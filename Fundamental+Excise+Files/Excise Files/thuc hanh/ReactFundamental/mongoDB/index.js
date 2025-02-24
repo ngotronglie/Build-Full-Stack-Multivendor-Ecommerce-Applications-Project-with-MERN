b7800ccf790ede6c48a21760fea4cb1b9b577fde
Liem <ngotrongliem2004@gmail.com>
2025-02-24 16:34:35 +0700

    shema and models

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/mongoDB/index.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/mongoDB/index.js
@@ -3,3 +3,25 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Database MongoDB Connected...!!!'))
   .catch(err => console.log(err));
+
+
+// schema (shape of the document)
+
+// document , collection, database 
+/*
+const user = [
+    { name: 'John', age: 25, email: 'BtB4e@example.com' },
+    { name: 'Jane', age: 24, email: 'oldfjg@example.com' },
+    { name: 'Jim', age: 26, email: 'BtB4e@example.com' }
+]
+*/
+
+const userSchema = new mongoose.Schema({
+ name: String,
+ age:Number,
+ isMarried: Boolean,
+ salary: Number,
+ gender: String,
+});
+
+const User = mongoose.model('User', userSchema);
