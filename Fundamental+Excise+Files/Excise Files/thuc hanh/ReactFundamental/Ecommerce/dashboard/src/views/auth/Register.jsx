cb701984775909a47d2c62a1944231d8639e2d82
Liem <ngotrongliem2004@gmail.com>
2025-02-28 19:32:05 +0700

    route and login register part 3

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/auth/Register.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/auth/Register.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
+            <div className='w-[350px] text-[#ffffff] p-2'>
+                <div className='bg-[#6f68d1] p-4 rounded-md'>
+                    <h2 className='text-xl mb-3 font-bold'>Welome to Ecommerce</h2>
+                    <p className='text-sm mb-3 font-medium'>please register your account</p>
+                    <form action="">
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="name">Name:</label>
+                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" placeholder='Enter your name' name='name'  id='name'  required />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
