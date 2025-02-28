86a396aab62b794a78eed31eb21140c9d8455ff2
Liem <ngotrongliem2004@gmail.com>
2025-02-28 21:59:47 +0700

    route and login register part 4

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/auth/Register.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/auth/Register.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Register = () => {
     return (
         <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
@@ -10,8 +10,43 @@ const Register = () => {
                     <form action="">
                         <div className='flex flex-col w-full gap-1 mb-3'>
                             <label htmlFor="name">Name:</label>
-                            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md' type="text" placeholder='Enter your name' name='name'  id='name'  required />
+                            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md'
+                                type="text" placeholder='Enter your name' name='name' id='name' required />
                         </div>
+
+                        {/*  */}
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="email">Email:</label>
+                            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md'
+                                type="email" placeholder='Enter your email' name='email' id='email' required />
+                        </div>
+
+                        {/*  */}
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <label htmlFor="password">Password:</label>
+                            <input className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md'
+                                type="password" placeholder='Enter your password' name='password' id='password' required />
+                        </div>
+                        {/*  */}
+
+                        <div className='flex items-center  w-full gap-1 mb-3'>
+                            <input type="checkbox" className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded forcus:ring-blue-500'
+                                name="checkbox" id="checkbox" />
+                            <label htmlFor="checkbox">I agree to privacy policy & treams</label>
+                        </div>
+                        {/*  */}
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <button className=' bg-slate-800 py-2 rounded-md hover:bg-slate-600  hover:shadow-blue-300 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Register</button>
+                        </div>
+
+
+                        <div className='flex flex-col w-full gap-1 mb-3'>
+                            <p>Already have an account? <Link className='text-blue-600' to={'/login'}>Login</Link></p>
+                        </div>
+
                     </form>
                 </div>
             </div>
