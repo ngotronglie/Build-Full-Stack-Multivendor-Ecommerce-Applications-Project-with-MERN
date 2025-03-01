956876fade1c7edffef800ae5e5e71d500754a28
Liem <ngotrongliem2004@gmail.com>
2025-03-01 09:10:43 +0700

    project redux toolkit setup part 1

---
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/store/Reducers/authReducer.js
@@ -0,0 +1,14 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const authReducer = createSlice({
+    name: 'auth',
+    initialState: {
+       successMessage: '',
+         errorMessage: '',
+         loader: false,
+         userInfor : ''
+    },
+    reducers: {},
+    extraReducers: {},
+})
+export default  authReducer.reducer;
