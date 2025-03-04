a8cd7532cade9c6efb03894898ee21b98b819e46
Liem <ngotrongliem2004@gmail.com>
2025-03-04 22:45:03 +0700

    project header design part 1

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/layout/Mainlayout.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/layout/Mainlayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import SideBar from './Sidebar';
@@ -11,10 +11,14 @@ import SideBar from './Sidebar';
 */}
 
 const MainLayout = () => {
+
+    const [showSidebar, setShowSidebar] = useState(false);
+
+
     return (
         <div className='bg-[#cdcae9] w-full min-h-screen'>
-            <Header />
-            <SideBar />
+            <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+            <SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
             <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
                 <Outlet />
             </div>
--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/layout/Sidebar.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/layout/Sidebar.jsx
@@ -37,7 +37,7 @@ const SideBar = () => {
                                 </Link>
                             </li>)
                         }
-                        <li >
+                        <li >  
                             <button className=' text-[#030811] font-bold duration-200
                                  px-[12px] py-[9px] rounded-sm flex justify-start items-center 
                                  gap-[12px] hover:pl-4 transition-all w-full mb-1'><span><BiLogOut /></span> <span>Logout</span></button>
