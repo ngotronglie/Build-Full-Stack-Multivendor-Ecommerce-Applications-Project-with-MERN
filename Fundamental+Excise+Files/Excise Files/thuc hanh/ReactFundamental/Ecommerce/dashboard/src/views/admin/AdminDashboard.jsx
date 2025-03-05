33ef8ef6e6f55b8ebd3f91a0efcd0960108cb3ba
Liem <ngotrongliem2004@gmail.com>
2025-03-05 23:21:10 +0700

    dasboard design part 1

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/AdminDashboard.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
-
-
+import { MdCurrencyExchange } from "react-icons/md";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { FaCartArrowDown } from "react-icons/fa";
 const AdminDashboard = () => {
     return (
-        <div>AdminDashboard</div>
+        <div className='px-2 md:px-7 py-5'>
+            <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7'>
+
+
+                {/* ------------------------------------------------- */}
+                <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+                        <h2 className='text-3lx font-bold'>$ 1000</h2>
+                        <span className='text-md font-medium'>Total sale</span>
+                    </div>
+
+                    <div className='w-[40px] h-[47px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl'>
+                        <MdCurrencyExchange className='text-[#fae8e8] shadow-lg' />
+                    </div>
+                </div>
+                {/* ------------------------------------------------- */}
+                <div className='flex justify-between items-center p-5 bg-[#fde2ff] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+                        <h2 className='text-3lx font-bold'>50</h2>
+                        <span className='text-md font-medium'>Products</span>
+                    </div>
+
+                    <div className='w-[40px] h-[47px] rounded-full bg-[#750077] flex justify-center items-center text-xl'>
+                        <MdProductionQuantityLimits className='text-[#fde2ff] shadow-lg' />
+                    </div>
+                </div>
+                {/* ------------------------------------------------- */}
+                <div className='flex justify-between items-center p-5 bg-[#e9feea] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+                        <h2 className='text-3lx font-bold'>60</h2>
+                        <span className='text-md font-medium'>Sellers</span>
+                    </div>
+
+                    <div className='w-[40px] h-[47px] rounded-full bg-[#038000] flex justify-center items-center text-xl'>
+                        <FaUsers className='text-[#fae8e8] shadow-lg' />
+                    </div>
+                </div>
+                {/* ------------------------------------------------- */}
+                <div className='flex justify-between items-center p-5 bg-[#ecebff] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+                        <h2 className='text-3lx font-bold'>3030</h2>
+                        <span className='text-md font-medium'>orders</span>
+                    </div>
+
+                    <div className='w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl'>
+                        <FaCartArrowDown className='text-[#fae8e8] shadow-lg' />
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
     )
 }
 
