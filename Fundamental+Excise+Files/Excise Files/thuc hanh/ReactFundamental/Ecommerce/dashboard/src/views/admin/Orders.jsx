f082f8790c4c5b7695661222888c411d5a3dbcba
Liem <ngotrongliem2004@gmail.com>
2025-03-08 22:57:31 +0700

    Admin order page design part 2

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/Orders.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/Orders.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BsBoxArrowInDown } from "react-icons/bs";
 
 const Orders = () => {
 
@@ -17,6 +18,32 @@ const Orders = () => {
                     </select>
 
                     <input type="text" placeholder="search..." className="px-4 py-2 forcus:border-indigo-500 outline-none bg-[#283046]  border border-slate-700 rounded-md text-[#d0d2d6]" />
+                </div>
+
+                <div className="relative mt-5 overflow-x-auto">
+                    <div className="w-full text-sm text-left text-gray-500 ">
+                        <div className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
+                            <div className="flex justify-between items-center">
+                                <div className="py-3 w-[25%] font-bold">Order id</div>
+                                <div className="py-3 w-[13%] font-bold">price</div>
+                                <div className="py-3 w-[18%] font-bold">payment status</div>
+                                <div className="py-3 w-[18%] font-bold">Order status</div>
+                                <div className="py-3 w-[18%] font-bold">Action</div>
+                                <div className="py-3 w-[8%] font-bold"><BsBoxArrowInDown /></div>
+                            </div>
+                        </div>
+                        {/* ********************************************************************** */}
+                        <div className=" text-[#d0d2d6] ">
+                            <div className="flex justify-between items-start border-b border-slate-700">
+                                <div className="py-3 w-[25%] font-medium whitespace-nowrap">#11111</div>
+                                <div className="py-3 w-[13%] font-medium">$5456</div>
+                                <div className="py-3 w-[18%] font-medium">pending</div>
+                                <div className="py-3 w-[18%] font-medium">pending</div>
+                                <div className="py-3 w-[18%] font-medium">view</div>
+                                <div className="py-3 w-[8%] font-medium"><BsBoxArrowInDown /></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
