37eac964e4e1492f8effe43f36c2ff6a7e749b6e
Liem <ngotrongliem2004@gmail.com>
2025-03-10 23:45:35 +0700

    Sellers page design

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/router/routes/adminRoutes.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/router/routes/adminRoutes.js
@@ -2,7 +2,7 @@ import {lazy} from 'react';
 import Category from '../../views/admin/Category';
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard') )
 const Orders = lazy(() => import('./../../views/admin/Orders') )
-
+const Sellers = lazy(() => import('./../../views/admin/Sellers') )
 export const adminRoutes = [
     {
         path : 'admin/dashboard',
@@ -17,6 +17,11 @@ export const adminRoutes = [
     {
         path : 'admin/dashboard/category',
         element: <Category/>,
+        role: 'admin'
+    },
+    {
+        path : 'admin/dashboard/sellers',
+        element: <Sellers/>,
         role: 'admin'
     },
 ]
