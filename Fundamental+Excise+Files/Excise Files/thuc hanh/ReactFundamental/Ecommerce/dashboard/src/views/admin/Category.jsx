c16d2361aaa30ed4e43e0d187343da5cce1f67e2
Liem <ngotrongliem2004@gmail.com>
2025-03-10 23:25:54 +0700

    Category page design part 5

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/Category.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/Category.jsx
@@ -4,6 +4,7 @@ import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 import Pagination from "../Pagination";
 import { Link } from "react-router-dom";
 import { FaEdit, FaImage, FaTrash } from "react-icons/fa";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Category = () => {
 
@@ -14,6 +15,17 @@ const Category = () => {
 
     return (
         <div className='px-2 lg:px-7 pt-5'>
+
+            <div className="flex lg:hidden justify-between items-center mb-5 p-4 bg-[#6a5fdf] rounded-md">
+                <h1 className="text-lg font-semibold">Category</h1>
+                <button onClick={() => setShow(true)} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer text-sm">
+                    Add
+                </button>
+            </div>
+
+
+
+
             <div className='flex flex-wrap w-full'>
                 <div className='w-full lg:w-7/12'>
                     <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -86,13 +98,20 @@ const Category = () => {
                 <div
                     className={`w-[320px] lg:w-5/12 translate-x-100 lg:relative lg:right-0 fixed 
                                 ${show ? 'right-0' : '-right-[340px]'} 
-                                    z-20 top-0 transition-all duration-500`}
+                                    z-[9999] top-0 transition-all duration-500`}
                 >
                     <div className="w-full pl-5">
                         <div className="bg-[#6a5fdf] h-screen lg:h-auto px-3 py-2 lg:rounded-md text-[#d0d2d6]">
-                            <h1 className="text-[#d0d2d6] font-semibold text-xl mb-4 w-full text-center">
-                                Add Category
-                            </h1>
+                            <div className="flex justify-between items-center mb-4">
+                                <h1 className="text-[#d0d2d6] font-semibold text-xl mb-4 w-full text-center">
+                                    Add Category
+                                </h1>
+                                <div onClick={() => setShow(false)} className="block lg:hidden cursor-pointer text-gray-600 hover:text-gray-800 transition">
+                                    <IoMdCloseCircle size={24} />
+                                </div>
+                            </div>
+
+
                             <form>
                                 <div className="flex flex-col w-full gap-1 mb-3">
                                     <label htmlFor="name">Category Name</label>
