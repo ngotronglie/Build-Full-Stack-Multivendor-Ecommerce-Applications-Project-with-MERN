5b1ae7ce5c5209d2a48507254ff33530afec6eab
Liem <ngotrongliem2004@gmail.com>
2025-03-11 22:48:20 +0700

    payment request page design 2

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/PaymentRequest.jsx
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/views/admin/PaymentRequest.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
-import { FixedSizeList } from 'react-window';
+import { FixedSizeList as List } from 'react-window';
+import { forwardRef } from "react";
+
+function handleOnWheel(event) {
+    const { deltaY } = event;
+    console.log("handleOnWheel", deltaY);
+}
+
+const outerElementType = forwardRef((props, ref) => (
+    <div ref={ref} onWheel={handleOnWheel} {...props} />
+));
+
+
 
 const PaymentRequest = () => {
+
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    const Row = ({ index, style }) => {
+        return (
+            <div style={style} className="flex text-sm font-medium text-[#d0d2d6]">
+                <div className="w-[25%] p-2 whitespace-nowrap">{index + 1}</div>
+                <div className="w-[25%] p-2 whitespace-nowrap">$3434</div>
+                <div className="w-[25%] p-2 whitespace-nowrap">
+                    <span className="py-[1px] px-[5px] bg-slate-700 text-blue-500 rounded-md text-sm">
+                        Pending
+                    </span>
+                </div>
+                <div className="w-[25%] p-2 whitespace-nowrap">25 dec 2025</div>
+                <div className="w-[25%] p-2 whitespace-nowrap">
+                    <button
+                        className="
+                                 bg-indigo-500 shadow-lg 
+                                  hover:shadow-indigo-500/50 
+                                     px-3 py-[2px] cursor-pointer 
+                                     text-white rounded-sm text-sm
+                                    "
+                    >
+                        Confirm
+                    </button>
+
+                </div>
+            </div>
+        );
+    };
+
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
@@ -17,6 +60,20 @@ const PaymentRequest = () => {
                             <div className="w-1/5 p-2">Date</div>
                             <div className="w-1/5 p-2">Action</div>
                         </div>
+                        {
+                            <List
+                                style={{ minWidth: '340px' }}
+                                className="List"
+                                height={350}
+                                itemCount={10}
+                                itemSize={35}
+                                outerElementType={outerElementType}
+                            >
+                                {({ index, style }) => <Row index={index} style={style} />}
+                            </List>
+
+
+                        }
                     </div>
                 </div>
 
