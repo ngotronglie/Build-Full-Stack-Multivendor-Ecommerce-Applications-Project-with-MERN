45f36f37e6783677d2c5084b645f0a60a9f0817a
Liem <ngotrongliem2004@gmail.com>
2025-03-13 17:16:17 +0700

    Add product page design part 1

--- Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/router/routes/sellerRoutes.js
+++ Fundamental+Excise+Files/Excise Files/thuc hanh/ReactFundamental/Ecommerce/dashboard/src/router/routes/sellerRoutes.js
@@ -1,6 +1,7 @@
 import {lazy} from 'react';
 const Home = lazy(()=>import('./../../views/Home') )
 const SellerDashboard = lazy(()=>import('./../../views/seller/SellerDashboard') )
+const AddProduct = lazy(()=>import('./../../views/seller/AddProduct') )
 
 
 
@@ -9,9 +10,15 @@ export const sellerRoutes = [
         path : '/',
         element: <Home/>,
         ability: ['admin', 'seller']
-    },{
+    },
+    {
         path : '/seller/dashboard',
         element: <SellerDashboard/>,
+        ability: ['seller']
+    }
+    ,{
+        path : '/seller/dashboard/add-product',
+        element: <AddProduct/>,
         ability: ['seller']
     }
 ]
